0ff61151221384c630486a8e50358bdf5b649ea0
oucel <omerucel@gmail.com>
2013-08-08 01:13:57 +0300

    set environment

--- vagrant/puppet/modules/basic/manifests/init.pp
+++ vagrant/puppet/modules/basic/manifests/init.pp
@@ -2,4 +2,11 @@ class basic{
 	group{ "puppet" :
 		ensure => present,
 	}
+
+	exec { "set_environments":
+		notify => Service["mysql"],
+		command => "echo LC_ALL='en_US.UTF-8' >> /etc/environment",
+		require => Package["mysql-server"],
+		onlyif => "grep -c '^LC_ALL' /etc/environment | grep 0",
+	}
 }
